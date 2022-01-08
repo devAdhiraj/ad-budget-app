f2169580c39b9ef0df9e6c76baca190016df389c
devAdhiraj <adhiraj.bal@gmail.com>
2022-01-07 19:10:29 -0500

    git debugging v3

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,6 +2,8 @@
 // import { getAuth } from 'firebase-admin/auth';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import refreshJwt from '../../helpers/refresh-jwt';
+import FirebaseServer from "../../helpers/firebase-server"
+
 // const Cookies = require("cookies")
 // const jwt = require("jsonwebtoken")
 
@@ -17,6 +19,7 @@ const handler = async (
       return res.status(404).json({result: "invalid http method"});
     }
     try{
+      FirebaseServer()
       if(await refreshJwt(req, res) !== "unauthorized"){
         return res.status(200).json({result: "success"})
       }
@@ -26,7 +29,7 @@ const handler = async (
         console.log(process.env.CLIENT_ID)
         console.log(process.env.AUTH_URI)
         console.log(process.env.CLIENT_CERT_URL)
-        
+
         return res.status(403).json({result: "unauthorized"})
       }
     } catch(err){
